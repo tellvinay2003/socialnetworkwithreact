9949d66e1a1ba3c18ae7e46ae0b0f40c2463cb86
vinay kumar pandey <tellvinay2003@yahoo.co.in>
2022-10-10 13:44:32 +0530

    EventDashBoard created

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,32 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
 
-function App() {
+
+
+// function App() {
+
+//   // const title = React.createElement('h1', {}, 'Revents no JSX');
+//   // const div = React.createElement('div', {className: 'App'}, title);
+//   // return div;
+
+//   return (
+//     <div className="App" style={{marginTop: '20px'}}>
+//       <h1>Re-vents</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+export default function App() {
   return (
     <div className="App">
-      <h>Re-vents</h>
+      <h1>Re-vents</h1>
+      <button className="ui icon red button">
+        <i className="user icon" />CSS Button
+      </button>
+      <Button icon='user' content='React Button' color="green" loading='true' />
     </div>
   );
-}
-
-export default App;
+} 
